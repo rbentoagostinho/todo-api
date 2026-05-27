d5f84d7d4347973f993685eebda81efca364ff8f
rbentoagostinho <ricardobento_85@hotmail.com>
2026-05-27 13:02:28 -0300

    Adiciona Services e interfaces

---
+++ TodoAPI/TodoAPI/Services/Interfaces/ITarefaService.cs
@@ -0,0 +1,13 @@
+﻿using TodoAPI.DTOs;
+
+namespace TodoAPI.Services.Interfaces;
+
+public interface ITarefaService
+{
+    Task<IEnumerable<TarefaDTO>> GetAllAsync();
+    Task<IEnumerable<TarefaDTO>> GetByFiltroAsync(TarefaFiltroDTO filtro);
+    Task<TarefaDTO?> GetByIdAsync(int id);
+    Task<TarefaDTO> CreateAsync(TarefaDTO dto);
+    Task<bool> UpdateAsync(int id, TarefaDTO dto);
+    Task<bool> DeleteAsync(int id);
+}
