b4822a39e5c79ba4805cd601a026661b4ac5fbbd
rbentoagostinho <ricardobento_85@hotmail.com>
2026-05-27 13:39:34 -0300

    configura Program.cs, AutoMapper, Swagger, appsettings e protege credenciais do banco

--- TodoAPI/TodoAPI/Program.cs
+++ TodoAPI/TodoAPI/Program.cs
@@ -1,11 +1,61 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using TodoAPI.Data;
+using TodoAPI.DTOs.Mappings;
+using TodoAPI.Repositories;
+using TodoAPI.Repositories.Interfaces;
+using TodoAPI.Services;
+using TodoAPI.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Todo API",
+        Version = "v1",
+        Description = "API RESTful para gerenciamento de tarefas",
+        Contact = new OpenApiContact
+        {
+            Name = "Seu Nome",
+            Email = "seu@email.com"
+        }
+    });
+});
+
+// Database
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+// AutoMapper
+builder.Services.AddSingleton<IMapper>(sp =>
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.AddProfile<MappingProfile>();
+    });
+    config.AssertConfigurationIsValid();
+    return config.CreateMapper();
+});
+
+// Repositories
+builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+
+// Services
+builder.Services.AddScoped<ITarefaService, TarefaService>();
 
 var app = builder.Build();
 
@@ -13,11 +63,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
